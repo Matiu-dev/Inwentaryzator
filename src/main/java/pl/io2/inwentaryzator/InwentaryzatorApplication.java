4f751eb69411f3c78b913d1fdb764bc20fcba1be
mwroblewski-at-596854579163 <S0dnO6NaXKpyiRDM0szh0novLXuFYiyt+ELGVwgREII=>
2020-12-11 20:44:50 +0100

    initial commit

---
+++ src/main/java/pl/io2/inwentaryzator/InwentaryzatorApplication.java
@@ -0,0 +1,13 @@
+package pl.io2.inwentaryzator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InwentaryzatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InwentaryzatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/io2/inwentaryzator/InwentaryzatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.io2.inwentaryzator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InwentaryzatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
